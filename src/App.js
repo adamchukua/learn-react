da60168abcc98fc27da228105b75c5d646246d2b
thegradle <adamchuk.v.1@gmail.com>
2023-02-17 13:01:17 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import Card from "./components/Card"
-import data from "./data.js"
-import "./styles.css"
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
-  const cards = data.map(item => <Card {...item}/>)
-
+function App() {
   return (
-    <div>
-      <Navbar/>
-      <div className="container">
-        {cards}
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
+
+export default App;
