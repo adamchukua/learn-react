e4f3cef2c32e28a722ec603c9b5a99c43c0b0b59
thegradle <adamchuk.v.1@gmail.com>
2023-02-17 11:39:08 +0200

    Add badge for Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,25 @@
 import React from "react"
 
 export default function Card(props) {
+  let badgeText
+
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT"
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE"
+  }
+
   return (
     <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+
       <img src={`./images/${props.img}`} alt="" className="card--img"/>
 
       <div className="card-review">
         <img src="./images/star.svg" alt="star" className="card-review--img"/>
 
         <p className="card-review--text">
-          <span className="card-review--text__black">{props.rating}</span> <span>({props.reviewCount})</span> • <span>{props.country}</span>
+          <span className="card-review--text__black">{props.rating}</span> <span>({props.reviewCount})</span> • <span>{props.location}</span>
         </p>
       </div>
 
