008cf0e8085aeadd37b267316defd102b8036203
thegradle <adamchuk.v.1@gmail.com>
2023-02-20 11:34:35 +0200

    Meme components with get random meme

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,20 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
+import memesData from "../memesData"
 
 export default function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg" 
+  })
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNumber].url
+    setMeme(prevMeme => ({
+        ...prevMeme,
+        randomImage: url
+    }))
+  }
+
   return (
-    <main>
-      <form className="meme-form container">
+    <main className="container">
+      <form className="meme-form">
         <div className="meme-form-inputs">
           <input type="text" placeholder="Top text"></input>
           <input type="text" placeholder="Bottom text"></input>
         </div>
 
-        <button className="meme-form--btn">
+        <button className="meme-form--btn" onClick={getMemeImage} type="button">
           Get a new meme image <img src="./images/capture-emoji.png" alt=""/>
         </button>
       </form>
 
-      <img src="" alt="" className="meme--img"/>
+      <img src={meme.randomImage} alt="" className="meme--img"/>
     </main>
   )
 }
